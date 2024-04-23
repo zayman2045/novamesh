f071ef82616708671f915d92c90c0647bdb89ffc
zayman2045 <xavierorlandogriffith@gmail.com>
2024-04-23 19:07:49 -0400

    Refactor layout.tsx to include EthereumProvider and rearrange components

--- nextjs-novamesh/app/layout.tsx
+++ nextjs-novamesh/app/layout.tsx
@@ -19,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <EthereumProvider>{children}</EthereumProvider>
+        <EthereumProvider>
+          <NavBar />
+          {children}
+        </EthereumProvider>
       </body>
     </html>
   );
