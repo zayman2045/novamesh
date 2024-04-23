a25a0603e0a8e704c5431bd41b48dbf8070391f3
zayman2045 <xavierorlandogriffith@gmail.com>
2024-04-23 19:05:59 -0400

    Add NavBar component to layout.tsx and update homepage title in page.tsx

--- nextjs-novamesh/app/layout.tsx
+++ nextjs-novamesh/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { EthereumProvider } from "./components/EthereumProvider";
 import "./globals.css";
+import NavBar from "./components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar />
         <EthereumProvider>{children}</EthereumProvider>
       </body>
     </html>
--- nextjs-novamesh/app/page.tsx
+++ nextjs-novamesh/app/page.tsx
@@ -1,12 +1,9 @@
-"use client";
-
-import EthereumConnectButton from "./components/EthereumConnectButton";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <h1>NovaMesh</h1>
-      <EthereumConnectButton />
+      <h1>NovaMesh Homepage</h1>
     </div>
   );
 }
