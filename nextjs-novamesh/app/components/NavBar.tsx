c95293b42ab0e586ee2a4d035a56fd3f33de388b
zayman2045 <xavierorlandogriffith@gmail.com>
2024-04-23 19:05:37 -0400

    Add NavBar component to the project

---
+++ nextjs-novamesh/app/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="flex items-center justify-between p-6 bg-purple-500">
+      <Link href="/">
+        <h1 className="text-2xl text-white">NovaMesh</h1>
+      </Link>
+      <div className="flex justify-around flex-grow">
+        <Link href="/explore">
+          <p className="text-white hover:text-purple-300 cursor-pointer">
+            Explore
+          </p>
+        </Link>
+        <Link href="/swap">
+          <p className="text-white hover:text-purple-300 cursor-pointer">
+            Swap
+          </p>
+        </Link>
+        <Link href="/nfts">
+          <p className="text-white hover:text-purple-300 cursor-pointer">
+            NFTs
+          </p>
+        </Link>
+      </div>
+      <button className="bg-white text-purple-500 hover:bg-purple-300 hover:text-white py-2 px-4 rounded">
+        Connect
+      </button>
+    </nav>
+  );
+}
