6315617f0aaededaf6c6931b72bc6dbc85e078ea
zayman2045 <xavierorlandogriffith@gmail.com>
2024-04-23 20:54:23 -0400

    Write test for counter account initialization.

--- anchor-novamesh/tests/anchor-novamesh.ts
+++ anchor-novamesh/tests/anchor-novamesh.ts
@@ -1,16 +1,26 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorNovamesh } from "../target/types/anchor_novamesh";
+import { expect } from "chai";
 
 describe("anchor-novamesh", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorNovamesh as Program<AnchorNovamesh>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const counter = anchor.web3.Keypair.generate();
+
+  it("Is initialized", async () => {
+    const tx = await program.methods
+      .initialize()
+      .accounts({ counter: counter.publicKey })
+      .signers([counter])
+      .rpc();
+
+    const account = await program.account.counter.fetch(counter.publicKey);
+    expect(account.count.toNumber()).to.equal(0);
   });
 });
