48521735cd28ab543ffbc793329c78a07177759b
zayman2045 <xavierorlandogriffith@gmail.com>
2024-04-30 07:47:49 -0400

    Add counter components to the program page

--- nextjs-novamesh/app/programs/page.tsx
+++ nextjs-novamesh/app/programs/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { useWallet } from "@solana/wallet-adapter-react";
+import CounterState from "../anchor-counter/components/CounterState";
+import IncrementButton from "../anchor-counter/components/IncrementButton";
 
 export default function ProgramsHome() {
   const wallet = useWallet();
   return (
     <>
-      <div className={"flex flex-col mx-auto border w-1/2 mt-4"}>
-        <h2 className={"mx-auto"}>Solana Counter</h2>
+      <div className={"flex flex-col mx-auto border rounded w-1/4 mt-4"}>
+        <h2 className={" bg-white text-black w-full"}>Solana Counter</h2>
         {wallet.connected ? (
-          <p> Wallet Connected </p>
+          <div className={"mx-auto"}>
+            <CounterState />
+            <IncrementButton />
+          </div>
         ) : (
           <p> Please Connect Wallet</p>
         )}
