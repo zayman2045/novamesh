3b03109fa37a5ae944912a58315f5a30c78a87c4
zayman2045 <xavierorlandogriffith@gmail.com>
2024-04-29 22:25:17 -0400

    Configure the counter program to use PDAs

--- anchor-novamesh/programs/anchor-counter/src/lib.rs
+++ anchor-novamesh/programs/anchor-counter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("DeffejzE94nG1u4ZoAhGzmS9bWfQ1Sd1RFknj7M2AJa5");
+declare_id!("dPRoKkdhQVbQfMtFkfXq1rjZM7ooCpnDpYhMBJGtwy4");
 
 #[program]
 pub mod anchor_counter {
@@ -8,38 +8,39 @@ pub mod anchor_counter {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
-        counter.count = 0;
-        msg!("Initializing counter");
+        counter.bump = ctx.bumps.counter;
+        msg!("Counter account created! Counter count: {}", counter.count);
+        msg!("Counter bump: {}", counter.bump);
         Ok(())
     }
 
     pub fn increment(ctx: Context<Update>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
-        msg!("Current count: {}", counter.count);
-        msg!("Incrementing count...");
-        counter.count += 1;
-        msg!("Updated count: {}", counter.count);
+        msg!("Previous count: {}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Count incremented! Current count: {}", counter.count);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
+    #[account(init, space = 8 + Counter::INIT_SPACE, seeds = [b"counter"], bump, payer = user)]
     pub counter: Account<'info, Counter>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-#[account]
-pub struct Counter {
-    pub count: u64,
-}
-
 #[derive(Accounts)]
 pub struct Update<'info> {
-    #[account(mut)]
+    #[account(mut, seeds = [b"counter"], bump = counter.bump)]
     pub counter: Account<'info, Counter>,
-    pub user: Signer<'info>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub count: u64,
+    pub bump: u8,
 }
--- anchor-novamesh/tests/anchor-counter.ts
+++ anchor-novamesh/tests/anchor-counter.ts
@@ -2,35 +2,40 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorCounter } from "../target/types/anchor_counter";
 import { assert, expect } from "chai";
+import { PublicKey } from "@solana/web3.js";
 
 describe("anchor-counter", () => {
-  // Configure the client to use the local cluster.
-
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorCounter as Program<AnchorCounter>;
 
-  const counter = anchor.web3.Keypair.generate();
+  const [counterPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("counter")],
+    program.programId
+  );
 
   it("Is initialized", async () => {
-    const tx = await program.methods
-      .initialize()
-      .accounts({ counter: counter.publicKey })
-      .signers([counter])
-      .rpc();
+    try {
+      const transactionSignature = await program.methods.initialize().rpc();
 
-    const account = await program.account.counter.fetch(counter.publicKey);
-    expect(account.count.toNumber()).to.equal(0);
+      const accountData = await program.account.counter.fetch(counterPDA);
+
+      console.log(`Transaction Signature: ${transactionSignature}`);
+      console.log(`Count: ${accountData.count}`);
+      expect(accountData.count.toNumber()).to.equal(0);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
   it("Incremented the count", async () => {
-    const tx = await program.methods
-      .increment()
-      .accounts({ counter: counter.publicKey })
-      .rpc();
+    const transactionSignature = await program.methods.increment().rpc();
 
-    const account = await program.account.counter.fetch(counter.publicKey);
-    assert.deepEqual(account.count.toNumber(), 1);
+    const accountData = await program.account.counter.fetch(counterPDA);
+
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
+    assert.deepEqual(accountData.count.toNumber(), 1);
   });
 });
