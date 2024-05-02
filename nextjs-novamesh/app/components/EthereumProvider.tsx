652df1cc697eb75e9bdc82f7c4f65f89196174bc
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-01 20:40:12 -0400

    Refactor EthereumProvider component

--- nextjs-novamesh/app/components/EthereumProvider.tsx
+++ nextjs-novamesh/app/components/EthereumProvider.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   FC,
   ReactNode,
@@ -10,41 +8,47 @@ import {
 } from "react";
 import { ethers } from "ethers";
 
+// Extend the global Window interface to include the ethereum property
 declare global {
   interface Window {
     ethereum: any;
   }
 }
 
-interface EthereumProviderProps {
-  children: ReactNode;
-}
-
+// Define the Ethereum context type
 interface EthereumContextType {
   provider: ethers.BrowserProvider | null;
   signer: ethers.JsonRpcSigner | null;
 }
 
+// Create Ethereum context with initial null value
 const EthereumContext = createContext<EthereumContextType | null>(null);
 
+// Custom hook for accessing Ethereum context
 export function useEthereum() {
   return useContext(EthereumContext);
 }
 
+// Props type for EthereumProvider component
+interface EthereumProviderProps {
+  children: ReactNode;
+}
+
+// EthereumProvider component to manage Ethereum-related state
 export const EthereumProvider: FC<EthereumProviderProps> = ({ children }) => {
   const [provider, setProvider] = useState<ethers.BrowserProvider | null>(null);
   const [signer, setSigner] = useState<ethers.JsonRpcSigner | null>(null);
 
+  // Initialize provider and signer when the component mounts
   useEffect(() => {
     if (window.ethereum) {
       const newProvider = new ethers.BrowserProvider(window.ethereum);
       setProvider(newProvider);
-      newProvider.getSigner().then((newSigner) => {
-        setSigner(newSigner);
-      });
+      newProvider.getSigner().then(setSigner);
     }
   }, []);
 
+  // Provide the Ethereum context to child components
   return (
     <EthereumContext.Provider value={{ provider, signer }}>
       {children}
