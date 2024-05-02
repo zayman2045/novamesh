58d722624af9cf9aef7827826d5d55ab4f56eece
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-01 21:09:16 -0400

    Refactor EthereumConnectButton component to improve code readability and remove redundant code

--- nextjs-novamesh/app/components/EthereumConnectButton.tsx
+++ nextjs-novamesh/app/components/EthereumConnectButton.tsx
@@ -3,16 +3,20 @@
 import { useEthereum } from "./EthereumProvider";
 import { ethers } from "ethers";
 
+// Component for displaying a button to connect to an Ethereum wallet
 export default function EthereumConnectButton() {
+  // Retrieve Ethereum context with provider and signer
   const ethereum = useEthereum();
   let provider: ethers.BrowserProvider | null = null,
-    signer: ethers.JsonRpcSigner | null = null;
+      signer: ethers.JsonRpcSigner | null = null;
 
+  // Destructure provider and signer from the context if available
   if (ethereum !== null) {
     ({ provider, signer } = ethereum);
     console.log(provider, signer);
   }
 
+  // Function to initiate connection to the user's Ethereum wallet
   const connectWallet = async () => {
     if (provider === null) {
       console.error("Provider is null");
@@ -20,6 +24,7 @@ export default function EthereumConnectButton() {
     }
 
     try {
+      // Request account access to the user's Ethereum wallet
       const accounts = await provider.send("eth_requestAccounts", []);
       console.log("Connected account:", accounts[0]);
     } catch (error) {
@@ -27,6 +32,7 @@ export default function EthereumConnectButton() {
     }
   };
 
+  // Render a button to connect the wallet if no signer is available, else show connected status
   return (
     <>
       {signer ? (
