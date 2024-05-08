cf05372aad3cf44cc7e553c5fa0b1d21fdd618ca
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-07 22:02:09 -0400

    Add sepolia network configuration to hardhat.config.ts

--- hardhat-novamesh/hardhat.config.ts
+++ hardhat-novamesh/hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
