234915e59af7e163645fe77ee10a66256e19c3f0
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-08 22:12:58 -0400

    Refactor ProgramsHome component to use AnchorCounter and HardhatCounter components

--- nextjs-novamesh/app/programs/page.tsx
+++ nextjs-novamesh/app/programs/page.tsx
@@ -1,27 +1,11 @@
-"use client";
-
-import { useWallet } from "@solana/wallet-adapter-react";
-import AnchorCounterState from "./anchor-counter/components/AnchorCounterState";
-import HardhatCounterState from "./hardhat-counter/HardhatCounterState";
-import IncrementButton from "./anchor-counter/components/IncrementButton";
+import AnchorCounter from "./anchor-counter/components/AnchorCounter";
+import HardhatCounter from "./hardhat-counter/components/HardhatCounter";
 
 export default function ProgramsHome() {
-  const wallet = useWallet();
   return (
     <>
-      <div className={"flex flex-col mx-auto border rounded w-1/4 mt-4"}>
-        <h2 className={" bg-white text-black w-full"}>Solana Counter</h2>
-        {wallet.connected ? (
-          <div className={"mx-auto"}>
-            <AnchorCounterState />
-            <IncrementButton />
-          </div>
-        ) : (
-          <p> Please Connect Solana Wallet</p>
-        )}
-      </div>
-      <h2>Ethereum Counter</h2>
-      <HardhatCounterState />
+      <AnchorCounter/>
+      <HardhatCounter/>
     </>
   );
 }
