534bc78e276dbc1d39ddc4fb67ca1a3434b0a199
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-08 21:34:41 -0400

    Add ETHERSCAN_API_KEY to hardhat.config.ts

--- hardhat-novamesh/hardhat.config.ts
+++ hardhat-novamesh/hardhat.config.ts
@@ -4,6 +4,7 @@ import { vars } from "hardhat/config";
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -12,6 +13,9 @@ const config: HardhatUserConfig = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
 
