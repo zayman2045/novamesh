6856d329c696a606d3179a287788c6e1efe0f488
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-08 21:37:02 -0400

    Add HardhatCounterState component to handle reading counter state in HardhatCounterState.tsx

---
+++ nextjs-novamesh/app/programs/hardhat-counter/HardhatCounterState.tsx
@@ -0,0 +1,14 @@
+import { useReadContract } from "wagmi";
+import { useReadCounterSCounter, counterAbi } from "../../../src/generated";
+
+export default function HardhatCounterState() {
+  const result = useReadContract({
+    abi: counterAbi,
+    address: "0xA91FA55aE9cb5d1f4701177C99Cd7E418940CCa2",
+    functionName: "s_counter",
+  });
+
+  const another_result = useReadCounterSCounter();
+
+  return <div>Count: {result.data?.toString()}</div>;
+}
