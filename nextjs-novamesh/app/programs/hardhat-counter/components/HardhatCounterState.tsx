c654f947c3b078df3bf7f9cb01961130c9281223
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-09 00:50:31 -0400

    Refactor HardhatCounterState component to use useReadCounterSCounter from generated file

--- nextjs-novamesh/app/programs/hardhat-counter/components/HardhatCounterState.tsx
+++ nextjs-novamesh/app/programs/hardhat-counter/components/HardhatCounterState.tsx
@@ -1,14 +1,7 @@
-import { useReadContract } from "wagmi";
-import { useReadCounterSCounter, counterAbi } from "../../../../src/generated";
+import { useReadCounterSCounter } from "@/src/generated";
 
 export default function HardhatCounterState() {
-  const result = useReadContract({
-    abi: counterAbi,
-    address: "0xA91FA55aE9cb5d1f4701177C99Cd7E418940CCa2",
-    functionName: "s_counter",
-  });
-
-  const another_result = useReadCounterSCounter();
+  const result = useReadCounterSCounter();
 
   return <div>Count: {result.data?.toString()}</div>;
 }
