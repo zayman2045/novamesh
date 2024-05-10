c6c968be7234fb52fbe4e9d70ef0598e43486317
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-09 21:25:05 -0400

    Add comments

--- nextjs-novamesh/app/programs/anchor-counter/setup.ts
+++ nextjs-novamesh/app/programs/anchor-counter/setup.ts
@@ -2,16 +2,22 @@ import { IdlAccounts, Program } from "@coral-xyz/anchor";
 import { IDL, AnchorCounter } from "./idl";
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
+// Specify the devnet program ID for the Anchor Counter program.
 const programId = new PublicKey("dPRoKkdhQVbQfMtFkfXq1rjZM7ooCpnDpYhMBJGtwy4");
+
+// Establish a connection to the Solana devnet cluster.
 const connection = new Connection(clusterApiUrl("devnet"));
 
+// Create a new Program instance with the IDL and program ID.
 export const program = new Program<AnchorCounter>(IDL, programId, {
   connection,
 });
 
+// Derive the program-derived address for the "counter" account.
 export const [counterPDA] = PublicKey.findProgramAddressSync(
   [Buffer.from("counter")],
   programId
 );
 
+// Export the type for the 'counter' account data.
 export type CounterData = IdlAccounts<AnchorCounter>["counter"];
