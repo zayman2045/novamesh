7b4cdd544fa2fd5702dc426d1462f87824deb66f
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-11 11:43:15 -0400

    Remove unused code in Counter module

--- hardhat-novamesh/ignition/modules/Counter.ts
+++ hardhat-novamesh/ignition/modules/Counter.ts
@@ -3,7 +3,5 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("Counter", (m) => {
   const counter = m.contract("Counter", []);
 
-  m.call(counter, "increment", []);
-
   return { counter };
 });
