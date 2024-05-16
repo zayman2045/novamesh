be35d9070ba7d506a7fb789e2ec4fcaffb5e557f
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-15 23:52:43 -0400

    Add update student instruction and test

--- anchor-novamesh/programs/student_intro/src/lib.rs
+++ anchor-novamesh/programs/student_intro/src/lib.rs
@@ -19,12 +19,36 @@ pub mod student_intro {
         student.address = ctx.accounts.user.key();
         Ok(())
     }
+
+    pub fn update_student(
+        ctx: Context<UpdateStudent>,
+        name: String,
+        intro: String,
+        age: u8,
+    ) -> Result<()> {
+        let student = &mut ctx.accounts.student;
+        student.name = name;
+        student.intro = intro;
+        student.age = age;
+        student.address = ctx.accounts.user.key();
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 #[instruction(name: String, intro: String)]
 pub struct AddStudent<'info> {
     #[account(init, payer = user, space = 8 + 32 + 1 + 4 + name.len() + 4 + intro.len(), seeds = [name.as_bytes(), user.key().as_ref()], bump)]
+    pub student: Account<'info, Student>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String, intro: String)]
+pub struct UpdateStudent<'info> {
+    #[account(mut,  seeds = [name.as_bytes(), user.key().as_ref()], bump, realloc = 8 + 32 + 1 + 4 + name.len() + 4 + intro.len(), realloc::payer = user, realloc::zero = true)]
     pub student: Account<'info, Student>,
     #[account(mut)]
     pub user: Signer<'info>,
--- anchor-novamesh/tests/student-intro.ts
+++ anchor-novamesh/tests/student-intro.ts
@@ -4,27 +4,48 @@ import { StudentIntro } from "../target/types/student_intro";
 import { expect } from "chai";
 
 describe("student-intro", () => {
-    const provider = anchor.AnchorProvider.env();
-    anchor.setProvider(provider);
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-    const program = anchor.workspace.StudentIntro as Program<StudentIntro>;
+  const program = anchor.workspace.StudentIntro as Program<StudentIntro>;
 
-    const student = {
-        name: "Xavier",
-        intro: "I'm here to learn!",
-        age: 27,
-    }
+  const student = {
+    name: "Zay",
+    intro: "I'm here to learn!",
+    age: 27,
+  };
 
-    const [studentPDA] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from(student.name), provider.wallet.publicKey.toBuffer()], program.programId)
+  const [studentPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(student.name), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  );
 
+  it("initialize student", async () => {
+    const tx = await program.methods
+      .addStudent(student.name, student.intro, student.age)
+      .rpc();
+    console.log(`Transaction Signature: ${tx}`);
+    const account = await program.account.student.fetch(studentPDA);
+    expect(account.address.toBase58()).to.equal(
+      provider.wallet.publicKey.toBase58()
+    );
+    expect(account.name).to.equal(student.name);
+    expect(account.intro).to.equal(student.intro);
+    expect(account.age).to.equal(student.age);
+  });
 
-    it("initialize student", async () => {
-        const tx = await program.methods.addStudent(student.name, student.intro, student.age).rpc();
-        console.log(`Transaction Signature: ${tx}`);
-        const account = await program.account.student.fetch(studentPDA);
-        // expect(account.address.toBase58).to.equal(provider.wallet.publicKey.toBase58());
-        expect(account.name).to.equal(student.name);
-        expect(account.intro).to.equal(student.intro);
-        expect(account.age).to.equal(student.age);
-    })
-})
+  it("update student", async () => {
+    const newStudent = {
+      intro: "Hey hi hello",
+      age: 45,
+    };
+    const tx = await program.methods
+      .updateStudent(student.name, newStudent.intro, newStudent.age)
+      .rpc();
+    const account = await program.account.student.fetch(studentPDA);
+    expect(account.name).to.equal(student.name);
+    expect(account.age).to.equal(newStudent.age);
+    expect(account.intro).to.equal(newStudent.intro);
+    expect(account.address.toBase58()).to.equal(provider.wallet.publicKey.toBase58())
+  });
+});
