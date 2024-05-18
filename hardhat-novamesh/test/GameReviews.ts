1d0bc8a9406f45ab1f3bf9dc5310973bc1feca29
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-18 13:23:28 -0400

    Refactor game review tests and add new tests for update and delete operations

--- hardhat-novamesh/test/GameReviews.ts
+++ hardhat-novamesh/test/GameReviews.ts
@@ -32,9 +32,99 @@ describe("game reviews", () => {
     const reviews: GameReview[] = await gameReviews.getGameReviews();
     expect(reviews.length).to.equal(1);
 
-    const addedReview = reviews[0];
-    expect(addedReview.title).to.equal(inputReview.title);
-    expect(addedReview.description).to.equal(inputReview.description);
-    expect(addedReview.rating).to.equal(inputReview.rating);
+    const reviewResponse = reviews[0];
+    expect(reviewResponse.title).to.equal(inputReview.title);
+    expect(reviewResponse.description).to.equal(inputReview.description);
+    expect(reviewResponse.rating).to.equal(inputReview.rating);
+  });
+
+  it("game review updated", async () => {
+    const { gameReviews } = await loadFixture(setup);
+
+    const initialReview = {
+      title: "Inception",
+      description: "Pretty good",
+      rating: 9,
+    };
+
+    await gameReviews.addGameReview(
+      initialReview.title,
+      initialReview.description,
+      initialReview.rating
+    );
+
+    const updatedReview = {
+      title: initialReview.title,
+      description: "Overrated",
+      rating: 6,
+    };
+
+    await gameReviews.updateGameReview(
+      updatedReview.title,
+      updatedReview.description,
+      updatedReview.rating
+    );
+
+    const reviews: GameReview[] = await gameReviews.getGameReviews();
+    expect(reviews).to.have.length(1);
+
+    const reviewResponse = reviews[0];
+    expect(reviewResponse.title).to.equal(updatedReview.title);
+    expect(reviewResponse.description).to.equal(updatedReview.description);
+    expect(reviewResponse.rating).to.equal(updatedReview.rating);
+  });
+
+  it("game review deleted", async () => {
+    const { gameReviews } = await loadFixture(setup);
+
+    const inputReview = {
+      title: "The Dark Knight",
+      description: "Absolutely amazing!",
+      rating: 10,
+    };
+
+    await gameReviews.addGameReview(
+      inputReview.title,
+      inputReview.description,
+      inputReview.rating
+    );
+    await gameReviews.deleteGameReview(inputReview.title);
+    const reviews: GameReview[] = await gameReviews.getGameReviews();
+    expect(reviews).to.have.length(0);
+  });
+
+  it("second game review added", async () => {
+    const { gameReviews } = await loadFixture(setup);
+
+    const firstReview = {
+      title: "Avengers",
+      description: "Greatest Movie Ever!",
+      rating: 10,
+    };
+
+    await gameReviews.addGameReview(
+      firstReview.title,
+      firstReview.description,
+      firstReview.rating
+    );
+
+    const secondReview = {
+      title: "Dune",
+      description: "Incredible!",
+      rating: 10,
+    };
+
+    await gameReviews.addGameReview(
+      secondReview.title,
+      secondReview.description,
+      secondReview.rating
+    );
+    const reviews: GameReview[] = await gameReviews.getGameReviews();
+    expect(reviews.length).to.equal(2);
+
+    const reviewResponse = reviews[1];
+    expect(reviewResponse.title).to.equal(secondReview.title);
+    expect(reviewResponse.description).to.equal(secondReview.description);
+    expect(reviewResponse.rating).to.equal(secondReview.rating);
   });
 });
