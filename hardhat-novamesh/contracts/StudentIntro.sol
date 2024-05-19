350af90fb7528a48fa5631184a6b00d2811cc58d
zayman2045 <xavierorlandogriffith@gmail.com>
2024-05-19 11:11:58 -0400

    Add functionality to the StudentIntro contract, allowing the user to get, update and delete the student data

--- hardhat-novamesh/contracts/StudentIntro.sol
+++ hardhat-novamesh/contracts/StudentIntro.sol
@@ -17,7 +17,45 @@ contract StudentIntro {
         string memory intro,
         uint8 age
     ) public {
-        require(s_students[msg.sender].studentAddress == address(0), "Student has already been added");
+        require(
+            s_students[msg.sender].studentAddress == address(0),
+            "Student has already been added"
+        );
         s_students[msg.sender] = Student(name, intro, msg.sender, age);
+    }
+
+    function getStudent() public view returns (Student memory) {
+        require(
+            s_students[msg.sender].studentAddress == msg.sender,
+            "Student does not exist"
+        );
+        return s_students[msg.sender];
+    }
+
+    function updateStudent(
+        string memory name,
+        string memory intro,
+        uint8 age
+    ) public {
+        // Check if a student with the same address already exists
+        require(
+            s_students[msg.sender].studentAddress == msg.sender,
+            "Student does not exist"
+        );
+
+        // If the student exists, update their information
+        s_students[msg.sender].name = name;
+        s_students[msg.sender].intro = intro;
+        s_students[msg.sender].age = age;
+    }
+
+    function removeStudent() public {
+        require(
+            s_students[msg.sender].studentAddress == msg.sender,
+            "Student does not exist"
+        );
+
+        // If the student exists, delete their information
+        delete s_students[msg.sender];
     }
 }
