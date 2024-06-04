3701ed71cffe3c7d52fd2fced49604e4bc52d581
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-03 21:26:59 -0400

    Fix typo in initialSupply variable

--- hardhat-novamesh/contracts/MeshToken.sol
+++ hardhat-novamesh/contracts/MeshToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MeshToken is ERC20 {
-    constructor(uint256 initialSuppy) ERC20("Mesh", "MESH") {
-        _mint(msg.sender, initialSuppy);
+    constructor(uint256 initialSupply) ERC20("Mesh", "MESH") {
+        _mint(msg.sender, initialSupply);
     }
 }
