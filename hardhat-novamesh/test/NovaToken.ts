38e4dad243af9b57b33da8e6ec1a8189852f4d9b
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-06 19:16:16 -0400

    Explicitly request the balance of the deployer's address

--- hardhat-novamesh/test/NovaToken.ts
+++ hardhat-novamesh/test/NovaToken.ts
@@ -4,13 +4,13 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import NovaTokenModule from "../ignition/modules/NovaToken";
 
 describe("nova token", () => {
-    const setup = async () => {
-        return ignition.deploy(NovaTokenModule);
-    }
+  const setup = async () => {
+    return ignition.deploy(NovaTokenModule);
+  };
 
-    it("initial supply is 0", async () => {
-        const {novaToken} = await loadFixture(setup);
-        const [deployer] = await ethers.getSigners();
-        expect(await novaToken.balanceOf(deployer)).to.equal(0);
-    })
-})
+  it("initial supply is 0", async () => {
+    const { novaToken } = await loadFixture(setup);
+    const [deployer] = await ethers.getSigners();
+    expect(await novaToken.balanceOf(deployer.address)).to.equal(0);
+  });
+});
