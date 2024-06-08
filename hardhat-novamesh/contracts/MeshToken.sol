59d2de332ed70857bc8cd26d5cc24971ab0fd9ec
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-07 22:35:13 -0400

    Add public mint function to the MeshToken contract

--- hardhat-novamesh/contracts/MeshToken.sol
+++ hardhat-novamesh/contracts/MeshToken.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MeshToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Mesh", "MESH") {
-        _mint(msg.sender, initialSupply);
+    constructor(uint256 _initialSupply) ERC20("Mesh", "MESH") {
+        _mint(msg.sender, _initialSupply);
+    }
+
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount);
     }
 }
