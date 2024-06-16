0b89c0e04fd00675aeafe67ccd8238034ec73c61
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-16 08:57:09 -0400

    Modify the initial supply of tokens minted

--- hardhat-novamesh/ignition/modules/NovaExchange.ts
+++ hardhat-novamesh/ignition/modules/NovaExchange.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const NovaExchangeModule = buildModule("NovaExchangeModule", (m) => {
-  const novaExchange = m.contract("NovaExchange", [10 ** 18]);
+  const novaExchange = m.contract("NovaExchange", [1000]);
   return { novaExchange };
 });
 
