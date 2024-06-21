6d494dcf2183d783d6cde079ca22ab012c87bf81
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-21 08:41:54 -0400

    chore: Refactor Ethereum home page layout
    The code changes include:
    - Wrapping the content in a flex container
    - Adding headings for minting Nova Tokens and Nova NFTs

--- nextjs-novamesh/app/ethereum/page.tsx
+++ nextjs-novamesh/app/ethereum/page.tsx
@@ -3,8 +3,16 @@ import EthereumConnectButton from "../components/EthereumConnectButton";
 export default function EthereumHome() {
   return (
     <>
-      <h1>Ethereum Home</h1>
-      <EthereumConnectButton/>
+      <div className="flex flex-col items-center">
+        <h1>Ethereum Home</h1>
+        <EthereumConnectButton />
+      </div>
+      <div>
+        <h2>Mint Nova Tokens</h2>
+      </div>
+      <div>
+        <h2>Mint Nova NFTs</h2>
+      </div>
     </>
   );
 }
