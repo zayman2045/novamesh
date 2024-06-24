20daa82f4073f2b1c9614b758aaa963902615fe6
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-24 19:16:13 -0400

    Refactor Solana home page layout and add sections for connecting a Solana wallet, minting Mesh Tokens, and minting Mesh NFTs

--- nextjs-novamesh/app/solana/page.tsx
+++ nextjs-novamesh/app/solana/page.tsx
@@ -3,12 +3,18 @@ import SolanaConnectButton from "../components/SolanaConnectButton";
 export default function SolanaHome() {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1>Solana Home</h1>
-        <SolanaConnectButton />
+      <div className="flex flex-col items-center m-4">
+        <div className="flex flex-col items-center border border-custom-purple m-4 p-4 w-3/5">
+          <h1>Connect A Solana Wallet</h1>
+          <SolanaConnectButton />
+        </div>
+        <div className="flex justify-center border border-custom-purple w-3/5 m-4 p-4">
+          <h2>Mint Mesh Tokens</h2>
+        </div>
+        <div className="flex justify-center border border-custom-purple w-3/5 m-4 p-4">
+          <h2>Mint Mesh NFTs</h2>
+        </div>
       </div>
-      <h2>Mint Mesh Tokens</h2>
-      <h2>Mint Mesh NFTs</h2>
     </>
   );
 }
