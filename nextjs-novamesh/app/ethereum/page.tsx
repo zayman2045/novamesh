18920fa811fc41f67ccf472ce75e5bfcd8374676
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-24 18:46:38 -0400

    Refactor Ethereum home page layout and add headings for minting Nova Tokens and Nova NFTs

--- nextjs-novamesh/app/ethereum/page.tsx
+++ nextjs-novamesh/app/ethereum/page.tsx
@@ -3,15 +3,17 @@ import EthereumConnectButton from "../components/EthereumConnectButton";
 export default function EthereumHome() {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1>Ethereum Home</h1>
-        <EthereumConnectButton />
-      </div>
-      <div>
-        <h2>Mint Nova Tokens</h2>
-      </div>
-      <div>
-        <h2>Mint Nova NFTs</h2>
+      <div className="flex flex-col m-4 items-center">
+        <div className="flex flex-col items-center border border-blue-500 w-3/5 p-4 m-4">
+          <h1>Connect An Ethereum Wallet</h1>
+          <EthereumConnectButton />
+        </div>
+        <div className="flex border border-blue-500 justify-center w-3/5 p-4 m-4">
+          <h2>Mint Nova Tokens</h2>
+        </div>
+        <div className="flex border border-blue-500 justify-center w-3/5 p-4 m-4">
+          <h2>Mint Nova NFTs</h2>
+        </div>
       </div>
     </>
   );
