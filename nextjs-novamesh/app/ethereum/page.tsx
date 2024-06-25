c3fdfd8b441f88de69866701d9cd25aef03b625b
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-25 15:42:10 -0400

    Update the styling of the Ethereum and Solana pages

--- nextjs-novamesh/app/ethereum/page.tsx
+++ nextjs-novamesh/app/ethereum/page.tsx
@@ -3,18 +3,22 @@ import EthereumConnectButton from "../components/EthereumConnectButton";
 export default function EthereumHome() {
   return (
     <>
-      <div className="flex flex-col m-4 items-center">
-        <div className="flex flex-col items-center border-4 border-blue-500 rounded-md w-3/5 p-4 m-4 bg-blue-300">
-          <h1 className="text-2xl">Connect An Ethereum Wallet</h1>
+      <div className="flex flex-col m-4 items-center text-white">
+        <div className="flex flex-col items-center border-4 border-custom-blue rounded-md w-3/5 p-4 m-4 bg-blue-400">
+          <h1 className="text-2xl pb-3 font-bold">Ethereum Wallet</h1>
           <EthereumConnectButton />
         </div>
-        <div className="flex flex-col border-4 border-blue-500 rounded-md items-center w-3/5 p-4 m-4">
-          <h2 className="text-2xl">Nova Tokens</h2>
-          <button>Mint</button>
+        <div className="flex flex-col border-4 border-custom-blue rounded-md items-center w-3/5 p-4 m-4 bg-blue-400">
+          <h2 className="text-2xl pb-3 font-bold">Nova Tokens</h2>
+          <button className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105">
+            Mint
+          </button>
         </div>
-        <div className="flex flex-col border-4 border-blue-500 rounded-md items-center w-3/5 p-4 m-4">
-          <h2 className="text-2xl">Nova NFTs</h2>
-          <button className="text-white bg-blue-500 border-2 rounded-lg p-2">Mint</button>
+        <div className="flex flex-col border-4 border-custom-blue rounded-md items-center w-3/5 p-4 m-4 bg-blue-400">
+          <h2 className="text-2xl pb-3 font-bold">Nova NFTs</h2>
+          <button className="text-white bg-custom-blue border-2 border-custom-blue rounded-lg p-1 w-20 font-bold hover:scale-105">
+            Mint
+          </button>
         </div>
       </div>
     </>
--- nextjs-novamesh/app/solana/page.tsx
+++ nextjs-novamesh/app/solana/page.tsx
@@ -3,16 +3,18 @@ import SolanaConnectButton from "../components/SolanaConnectButton";
 export default function SolanaHome() {
   return (
     <>
-      <div className="flex flex-col items-center m-4">
-        <div className="flex flex-col items-center border-4 border-custom-purple rounded-md m-4 p-4 w-3/5">
-          <h1>Connect A Solana Wallet</h1>
+      <div className="flex flex-col items-center m-4 text-white">
+        <div className="flex flex-col items-center border-4 border-custom-purple rounded-md m-4 p-4 w-3/5 bg-purple-400">
+          <h1 className="text-2xl font-bold pb-3">Solana Wallet</h1>
           <SolanaConnectButton />
         </div>
-        <div className="flex justify-center border-4 border-custom-purple rounded-md w-3/5 m-4 p-4">
-          <h2>Mesh Tokens</h2>
+        <div className="flex flex-col items-center border-4 border-custom-purple rounded-md w-3/5 m-4 p-4 bg-purple-400">
+          <h2 className="text-2xl pb-3 font-bold">Mesh Tokens</h2>
+          <button className="bg-custom-purple w-20 font-bold border border-custom-purple rounded-lg p p-1 hover:scale-105">Mint</button>
         </div>
-        <div className="flex justify-center border-4 border-custom-purple rounded-md w-3/5 m-4 p-4">
-          <h2>Mesh NFTs</h2>
+        <div className="flex flex-col items-center border-4 border-custom-purple rounded-md w-3/5 m-4 p-4 bg-purple-400">
+          <h2 className="text-2xl font-bold pb-3">Mesh NFTs</h2>
+          <button className="bg-custom-purple w-20 font-bold border border-custom-purple rounded-lg p-1 hover:scale-105">Mint</button>
         </div>
       </div>
     </>
