c8abfc1cb44bb7d89c1562f69bca5d4ede8424a1
zayman2045 <xavierorlandogriffith@gmail.com>
2024-06-25 15:41:42 -0400

    Add bold styling to the navbar elements

--- nextjs-novamesh/app/components/NavBar.tsx
+++ nextjs-novamesh/app/components/NavBar.tsx
@@ -12,17 +12,17 @@ export default function NavBar() {
       </Link>
       <div className="flex justify-between space-x-8">
         <Link href="/ethereum">
-          <p className="text-white hover:text-blue-800 cursor-pointer pb-2 border-b-2 border-white hover:border-blue-800">
+          <p className="text-white font-semibold hover:text-blue-800 cursor-pointer pb-2 border-b-2 border-white hover:border-blue-800">
             Ethereum
           </p>
         </Link>
         <Link href="/solana">
-          <p className="text-white hover:text-purple-800 cursor-pointer pb-2 border-b-2 border-white hover:border-purple-800">
+          <p className="text-white font-semibold hover:text-purple-800 cursor-pointer pb-2 border-b-2 border-white hover:border-purple-800">
             Solana
           </p>
         </Link>
         <Link href="/bridge">
-          <p className="text-white hover:text-black cursor-pointer pb-2 border-b-2 border-white hover:border-black">
+          <p className="text-white font-semibold hover:text-black cursor-pointer pb-2 border-b-2 border-white hover:border-black">
             Bridge
           </p>
         </Link>
