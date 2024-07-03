75e28eb1fd7d18ac90e79b1150328166aff3563e
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-02 23:43:10 -0400

    Factor out the EthereumWallet into its own component

---
+++ nextjs-novamesh/app/ethereum/EthereumWallet.tsx
@@ -0,0 +1,12 @@
+import EthereumConnectButton from "../components/EthereumConnectButton";
+
+export default function EthereumWallet() {
+    return(
+        <>
+         <div className="flex flex-col items-center border-4 border-custom-blue rounded-md w-3/5 p-4 m-4 bg-blue-400">
+          <h1 className="text-2xl pb-3 font-bold">Ethereum Wallet</h1>
+          <EthereumConnectButton />
+        </div>
+        </>
+    )
+}
--- nextjs-novamesh/app/ethereum/page.tsx
+++ nextjs-novamesh/app/ethereum/page.tsx
@@ -1,5 +1,5 @@
-import EthereumConnectButton from "../components/EthereumConnectButton";
-
+"use client";
+import EthereumWallet from "./EthereumWallet";
 let nfts = [
   { name: "NOVA NFT1" },
   { name: "NOVA NFT2" },
@@ -10,10 +10,7 @@ export default function EthereumHome() {
   return (
     <>
       <div className="flex flex-col m-4 items-center text-white">
-        <div className="flex flex-col items-center border-4 border-custom-blue rounded-md w-3/5 p-4 m-4 bg-blue-400">
-          <h1 className="text-2xl pb-3 font-bold">Ethereum Wallet</h1>
-          <EthereumConnectButton />
-        </div>
+        <EthereumWallet/>
         <div className="flex flex-col border-4 border-custom-blue rounded-md items-center w-3/5 p-4 m-4 bg-blue-400">
           <h2 className="text-2xl pb-3 font-bold">Nova Tokens</h2>
           <button className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105">
