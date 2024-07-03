76f3b6bcafb39b547b65fa115e375ef6ba83fd23
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-03 08:47:25 -0400

    Update the styling of the homepage with ETH and SOL logos

--- nextjs-novamesh/app/page.tsx
+++ nextjs-novamesh/app/page.tsx
@@ -1,8 +1,44 @@
+import ethLogo from "@/public/ethereum-eth-logo.svg";
+import solLogo from "@/public/solana-sol-logo.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center m-4">
-      <h1 className={"text-center"}>Welcome to NovaMesh</h1>
-      <p>Connect your Ethereum and Solana Wallet to mint Tokens and NFTs</p>
+    <div className="flex flex-col m-10">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Welcome to NovaMesh</h1>
+        <p className="m-6">
+          Connect your Ethereum and Solana Wallet to mint Tokens and NFTs
+        </p>
+      </div>
+      <div>
+        <h2 className="text-center font-bold text-2xl">Ethereum</h2>
+        <div className="flex justify-evenly m-6">
+          <div className="w-1/3">
+            <Image
+              src={ethLogo}
+              alt="ETH Logo"
+              width="250"
+              className="mx-auto"
+            />
+          </div>
+          <p className="w-1/3">Description of Ethereum functionalities...</p>
+        </div>
+      </div>
+      <div>
+        <h2 className="text-center font-bold text-2xl">Solana</h2>
+        <div className="flex justify-evenly m-6">
+          <div className=" w-1/3">Description of Solana functionalities</div>
+          <div className="w-1/3">
+            <Image
+              src={solLogo}
+              alt="SOL Logo"
+              width="250"
+              className="mx-auto"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
