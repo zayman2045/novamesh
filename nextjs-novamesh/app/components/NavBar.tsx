4f6796b99aa053cda1ea95b74efbb3b3e4e6b297
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-04 12:58:33 -0400

    Update the UI styling, replacing the words in the navbar with logos

--- nextjs-novamesh/app/components/NavBar.tsx
+++ nextjs-novamesh/app/components/NavBar.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import novameshLogo from "@/public/novamesh-title-text.png";
 import githubLogo from "@/public/github-logo.png";
+import ethLogo from "@/public/ethereum-eth-logo.svg";
+import solLogo from "@/public/solana-sol-logo.png";
 
 export default function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full h-[4rem] flex justify-between items-center p-2 text-white">
+    <nav className="fixed top-0 left-0 w-full h-[4rem] flex justify-between items-center p-2 text-white text-lg">
       <Link href="/">
         <Image
           src={novameshLogo}
@@ -17,19 +19,25 @@ export default function NavBar() {
           height={100}
         />
       </Link>
-      <div className="flex justify-between space-x-8 mr-4">
-        <Link href="/ethereum">
-          <p className="font-semibold hover:text-custom-blue cursor-pointer  hover:border-blue-800">
-            Ethereum
-          </p>
+      <div className="flex justify-around w-1/4 gap-4">
+        <Link
+          href="/ethereum"
+          className="border-b-2 border-white border-opacity-0 hover:border-opacity-100 transition-colors duration-300 py-2"
+        >
+          <Image src={ethLogo} alt="ETH Logo" className="" width={50} />
         </Link>
-        <Link href="/solana">
-          <p className="font-semibold hover:text-custom-purple cursor-pointer">
-            Solana
-          </p>
+        <Link
+          href="/solana"
+          className="border-b-2 border-white border-opacity-0 hover:border-opacity-100 transition-colors duration-300 py-2"
+        >
+          {" "}
+          <Image src={solLogo} alt="SOL Logo" width={50} />
         </Link>
-        <a href="https://github.com/zayman2045/novamesh">
-          <Image src={githubLogo} alt="Github Logo" width={25} height={25} />
+        <a
+          href="https://github.com/zayman2045/novamesh"
+          className="border-b-2 border-white border-opacity-0 hover:border-opacity-100 transition-colors duration-300 py-2"
+        >
+          <Image src={githubLogo} alt="Github Logo" width={50} height={25} />
         </a>
       </div>
     </nav>
