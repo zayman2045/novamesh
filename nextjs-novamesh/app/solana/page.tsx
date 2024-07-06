1742b911afdbff9e30f257a745d62d92adec2e10
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-05 21:16:17 -0400

    Update Solana page layout and styling

--- nextjs-novamesh/app/solana/page.tsx
+++ nextjs-novamesh/app/solana/page.tsx
@@ -10,11 +10,15 @@ export default function SolanaHome() {
         </div>
         <div className="flex flex-col items-center border-4 border-custom-purple rounded-md w-3/5 m-4 p-4 bg-purple-400 bg-opacity-50 border-opacity-50">
           <h2 className="text-2xl pb-3 font-bold">Mesh Tokens</h2>
-          <button className="bg-custom-purple w-20 font-bold border border-custom-purple rounded-lg p p-1 hover:scale-105">Mint</button>
+          <button className="bg-custom-purple w-20 font-bold border border-custom-purple rounded-lg p p-1 hover:scale-105">
+            Mint
+          </button>
         </div>
         <div className="flex flex-col items-center border-4 border-custom-purple rounded-md w-3/5 m-4 p-4 bg-purple-400 bg-opacity-50 border-opacity-50">
           <h2 className="text-2xl font-bold pb-3">Mesh NFTs</h2>
-          <button className="bg-custom-purple w-20 font-bold border border-custom-purple rounded-lg p-1 hover:scale-105">Mint</button>
+          <button className="bg-custom-purple w-20 font-bold border border-custom-purple rounded-lg p-1 hover:scale-105">
+            Mint
+          </button>
         </div>
       </div>
     </>
