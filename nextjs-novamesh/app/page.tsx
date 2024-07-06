64cfa259f89a21912633b44ae775c09c4c33c163
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-05 21:17:00 -0400

    Update page layout and styling for Ethereum and Solana sections

--- nextjs-novamesh/app/page.tsx
+++ nextjs-novamesh/app/page.tsx
@@ -1,11 +1,31 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import ethLogo from "@/public/ethereum-eth-logo.svg";
 import solLogo from "@/public/solana-sol-logo.png";
 import Image from "next/image";
 
 export default function Home() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setIsScrolled(position > 275);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col m-10">
-      <div className="flex flex-col justify-center items-center h-screen">
+      <div
+        className={`flex flex-col justify-center items-center text-center h-screen transition-all duration-500 ${isScrolled ? "blur-sm" : ""}`}
+      >
         <h1 className="text-3xl font-bold">Welcome to NovaMesh</h1>
         <p className="m-6">
           Connect your Ethereum and Solana Wallet to mint Tokens and NFTs
@@ -22,13 +42,16 @@ export default function Home() {
               className="mx-auto"
             />
           </div>
-          <p className="w-1/3">Description of Ethereum functionalities...</p>
+          <p className="w-1/3">
+            Description of Ethereum functionalities. Ethereum is amazing! It is
+            so great, especially when it comes to placeholder text.
+          </p>
         </div>
       </div>
       <div>
         <h2 className="text-center font-bold text-2xl">Solana</h2>
         <div className="flex justify-evenly m-6">
-          <div className=" w-1/3">Description of Solana functionalities</div>
+          <div className="w-1/3">Description of Solana functionalities</div>
           <div className="w-1/3">
             <Image
               src={solLogo}
