cc35fc4d2a7706a6e32e06ccfe0a1ec9a2c4de93
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-31 08:39:37 -0400

    Read the NovaToken contract address from the frontend

--- nextjs-novamesh/app/ethereum/NovaTokenMint.tsx
+++ nextjs-novamesh/app/ethereum/NovaTokenMint.tsx
@@ -1,14 +1,16 @@
 import { useAccount } from "wagmi";
+import { useReadNovaExchangeNovaToken } from "@/src/generated";
 
 export default function NovaTokenMint() {
   const { address } = useAccount();
+  const novaToken = useReadNovaExchangeNovaToken();
   return (
     <>
       <div
         className={`flex flex-col border-4 bg-opacity-50 border-opacity-50 ${address ? "border-custom-blue" : "border-gray-600"} rounded-md items-center w-3/5 p-4 m-4 ${address ? "bg-blue-400" : "bg-gray-400"}`}
       >
         <h2 className="text-2xl pb-3 font-bold">Nova Tokens</h2>
-        {address ? <h3 className="mb-3">Current Balance:</h3> : <div></div>}
+        {address ? <h3 className="mb-3">Current Balance: {novaToken.data} </h3> : <div></div>}
         {address ? <button
           className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105"
           disabled
