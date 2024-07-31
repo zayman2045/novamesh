d03bf9af4893de0473903c0ee8962ebeb44290dd
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-31 08:38:13 -0400

    Remove Rocket contract and related files

--- hardhat-novamesh/ignition/modules/Rocket.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("Apollo", (m) => {
-  const contract = m.contract("Rocket", ["Space-X"], {
-    id: "Apollo_Space_X",
-  });
-  m.call(contract, "launch");
-  return { contract };
-});
