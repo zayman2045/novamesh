15868444f0592101617cef983f85279de8afc4ab
zayman2045 <xavierorlandogriffith@gmail.com>
2024-07-31 22:15:32 -0400

    Read the NOVA balance of the user and the Nova Exchange on the frontend

--- nextjs-novamesh/app/ethereum/NovaTokenMint.tsx
+++ nextjs-novamesh/app/ethereum/NovaTokenMint.tsx
@@ -1,23 +1,51 @@
 import { useAccount } from "wagmi";
-import { useReadNovaExchangeNovaToken } from "@/src/generated";
+import {
+  novaExchangeAddress,
+  useReadNovaExchangeNovaToken,
+  useReadErc20BalanceOf,
+  useReadErc20,
+} from "@/src/generated";
+import { sepolia } from "viem/chains";
 
 export default function NovaTokenMint() {
-  const { address } = useAccount();
-  const novaToken = useReadNovaExchangeNovaToken();
+  const { address: userAddress } = useAccount();
+  const { data: novaToken } = useReadNovaExchangeNovaToken();
+  
+  const { data: userNovaBalance } = useReadErc20BalanceOf({
+    address: novaToken,
+    args: [userAddress!],
+  });
+
+  const { data: exchangeNovaBalance } = useReadErc20BalanceOf({
+    address: novaToken,
+    args: [novaExchangeAddress[11155111]],
+  });
+  
+
   return (
     <>
       <div
-        className={`flex flex-col border-4 bg-opacity-50 border-opacity-50 ${address ? "border-custom-blue" : "border-gray-600"} rounded-md items-center w-3/5 p-4 m-4 ${address ? "bg-blue-400" : "bg-gray-400"}`}
+        className={`flex flex-col border-4 bg-opacity-50 border-opacity-50 ${userAddress ? "border-custom-blue" : "border-gray-600"} rounded-md items-center w-3/5 p-4 m-4 ${userAddress ? "bg-blue-400" : "bg-gray-400"}`}
       >
         <h2 className="text-2xl pb-3 font-bold">Nova Tokens</h2>
-        {address ? <h3 className="mb-3">Current Balance: {novaToken.data} </h3> : <div></div>}
-        {address ? <button
-          className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105"
-          disabled
-        >
-          Mint
-        </button> : <p>Connect Wallet to Access</p>}
-        
+        {userAddress && (
+          <div>
+            <h3 className="mb-3">Nova Token Address: {novaToken} </h3>
+            <h3 className="mb-3">User Nova Balance: {userNovaBalance?.toString()}</h3>
+            <h3 className="mb-3">Exchange Nova Balance: {exchangeNovaBalance?.toString()}</h3>
+
+          </div>
+        )}
+        {userAddress ? (
+          <button
+            className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105"
+            disabled
+          >
+            Mint
+          </button>
+        ) : (
+          <p>Connect Wallet to Access</p>
+        )}
       </div>
     </>
   );
