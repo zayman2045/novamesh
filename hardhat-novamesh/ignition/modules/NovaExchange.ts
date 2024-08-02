2f24232d4ed96bef814ae488e999c8220fd3e86b
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-01 22:34:11 -0400

    Remove NovaExchange and NovaToken contracts

--- hardhat-novamesh/ignition/modules/NovaExchange.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const NovaExchangeModule = buildModule("NovaExchangeModule", (m) => {
-  const novaExchange = m.contract("NovaExchange", [1000]);
-  return { novaExchange };
-});
-
-export default NovaExchangeModule;
