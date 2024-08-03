35e4fca21f2e3bdfcae9b315c8a31b040ad72f00
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-03 13:41:49 -0400

    Implement token minting on the frontend using a form and the generated useWriteNovaTokenMintTokens React hook

--- nextjs-novamesh/app/ethereum/NovaTokenMint.tsx
+++ nextjs-novamesh/app/ethereum/NovaTokenMint.tsx
@@ -1,5 +1,9 @@
 import { useAccount } from "wagmi";
-import { useReadNovaTokenBalanceOf } from "@/src/generated";
+import {
+  useReadNovaTokenBalanceOf,
+  useWriteNovaTokenMintTokens,
+} from "@/src/generated";
+import { parseEther, parseUnits } from "viem";
 
 export default function NovaTokenMint() {
   const { address: userAddress } = useAccount();
@@ -8,8 +12,21 @@ export default function NovaTokenMint() {
   const { data: userNovaBalance } = useReadNovaTokenBalanceOf({
     args: [userAddress!],
   });
-  console.log("User Nova Balance:", userNovaBalance);
-  
+
+  //
+  const {
+    data: hash,
+    writeContract: mintTokens,
+    error,
+  } = useWriteNovaTokenMintTokens();
+
+  // Mint Nova tokens to the user
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const novaTokenAmount = formData.get("novaTokenAmount") as string;
+    mintTokens({ value: parseUnits(novaTokenAmount, 0) });
+  };
 
   return (
     <>
@@ -17,19 +34,31 @@ export default function NovaTokenMint() {
         className={`flex flex-col border-4 bg-opacity-50 border-opacity-50 ${userAddress ? "border-custom-blue" : "border-gray-600"} rounded-md items-center w-1/2 p-4 m-4 ${userAddress ? "bg-blue-400" : "bg-gray-400"}`}
       >
         <h2 className="text-2xl pb-3 font-bold">Nova Tokens</h2>
-        {userAddress && (
-          <div>
-            {/* <h3 className="mb-3">Nova Token Address: {novaToken} </h3> */}
-            <h3 className="mb-3">Nova Balance: {userNovaBalance?.toString()}</h3>
-          </div>
-        )}
+
         {userAddress ? (
-          <button
-            className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105"
-            disabled
-          >
-            Mint
-          </button>
+          <>
+            <div>
+              <h3 className="mb-3">
+                Nova Balance: {userNovaBalance?.toString()}
+              </h3>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <input
+                name="novaTokenAmount"
+                type="text"
+                className="text-black mr-3 text-right"
+                placeholder="0"
+                required
+              ></input>
+              <button
+                type="submit"
+                className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105"
+              >
+                Mint
+              </button>
+              {hash && <div>Transaction Hash: {hash}</div>}
+            </form>
+          </>
         ) : (
           <p>Connect Wallet to Access</p>
         )}
