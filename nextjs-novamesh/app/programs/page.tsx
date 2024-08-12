837159e091a3fab622ab543db592ae19335dd424
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-11 20:56:44 -0400

    Use state to update the Tester component's property

--- nextjs-novamesh/app/programs/page.tsx
+++ nextjs-novamesh/app/programs/page.tsx
@@ -1,13 +1,19 @@
 "use client";
-import React from "react";
+
+import React, { useState } from "react";
 import Tester from "./tester";
 
 export default function Programs() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => setCount(count + 1);
+
   return (
     <>
       <div className="flex flex-col items-center">
         <h1 className="">Programs</h1>
-        <Tester id={18} />
+        <Tester id={count} />
+        <button onClick={handleClick}>+</button>
       </div>
     </>
   );
