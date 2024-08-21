604e8a66abce2ebfcd5c3ed23622a1a0b0061bae
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-20 23:43:00 -0400

    Refactor the Ethereum homepage

--- nextjs-novamesh/app/ethereum/page.tsx
+++ nextjs-novamesh/app/ethereum/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import EthereumWallet from "./EthereumWallet";
 import NovaTokenMint from "./NovaTokenMint";
-import NovaNftMint from "./NovaNftMint";
+import { useAccount } from "wagmi";
+
 
 export default function EthereumHome() {
+  const { address: userAddress } = useAccount();
+
   return (
     <>
-      <div className="flex flex-col m-4 items-center text-white pt-[4rem]">
-        <EthereumWallet />
-        <NovaTokenMint />
-        <NovaNftMint />
+      <div className="flex flex-col m-4 items-center justify-center h-[calc(100vh-4rem)] text-white">
+        <div className="h-[55vh]">
+          <div>
+            <button className={`rounded-2xl py-1 px-2 mx-2 my-1 ${userAddress ? "hover:bg-blue-400" : "disabled"}`}>Swap</button>
+            <button className="rounded-2xl py-1 px-2 mx-2 my-1">Pool</button>
+            <button className="rounded-2xl py-1 px-2 mx-2 my-1">Send</button>
+            <button className="rounded-2xl py-1 px-2 mx-2 my-1">NFTs</button>
+          </div>
+          <NovaTokenMint />
+        </div>
       </div>
     </>
   );
