26337e5f014f56de366de94b1455f018e85a748d
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-21 22:02:59 -0400

    Create components for the Pool, Send, and NFT functionalities

---
+++ nextjs-novamesh/app/ethereum/NovaSend.tsx
@@ -0,0 +1,6 @@
+export default function NovaSend() {
+    return (
+      <div className="flex flex-col border-4 bg-opacity-50 border-opacity-50 rounded-md items-center justify-center h-[50vh] w-[50vw] p-4  border-custom-blue bg-blue-400"></div>
+    );
+  }
+  
