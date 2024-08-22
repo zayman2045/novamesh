eacb289948484342769aeee9e673f8726cab24bb
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-21 22:03:45 -0400

    Conditionally render the new components using the tabState

--- nextjs-novamesh/app/ethereum/page.tsx
+++ nextjs-novamesh/app/ethereum/page.tsx
@@ -1,30 +1,36 @@
 "use client";
 
-import NovaTokenSwap from "./NovaTokenSwap";
+import NovaSwap from "./NovaSwap";
+import NovaNFTs from "./NovaNFTs";
+import NovaSend from "./NovaSend";
+import NovaPool from "./NovaPool";
+import NovaTabButton from "./NovaTabButton";
 import { NovaTab, useNovaTab } from "./NovaTabProvider";
 import { useAccount } from "wagmi";
-import NovaTabButton from "./NovaTabButton";
 import { useEffect } from "react";
 
 export default function EthereumHome() {
   const { address: userAddress } = useAccount();
-  const {tab: tabState, setTab} = useNovaTab();
+  const { tab: tabState, setTab } = useNovaTab();
 
   useEffect(() => {
-    !userAddress ? setTab(NovaTab.None) : setTab(NovaTab.Swap)
-  }, [userAddress])
+    !userAddress ? setTab(NovaTab.None) : setTab(NovaTab.Swap);
+  }, [userAddress]);
 
   return (
     <>
       <div className="flex flex-col m-4 items-center justify-center h-[calc(100vh-4rem)] text-white">
         <div className="h-[55vh]">
           <div>
-            <NovaTabButton tabValue={NovaTab.Swap}/>
-            <NovaTabButton tabValue={NovaTab.Pool}/>
-            <NovaTabButton tabValue={NovaTab.Send}/>
-            <NovaTabButton tabValue={NovaTab.NFTs}/>
+            <NovaTabButton tabValue={NovaTab.Swap} />
+            <NovaTabButton tabValue={NovaTab.Pool} />
+            <NovaTabButton tabValue={NovaTab.Send} />
+            <NovaTabButton tabValue={NovaTab.NFTs} />
           </div>
-          {userAddress &&  tabState == NovaTab.Swap && <NovaTokenSwap />}
+          {userAddress && tabState == NovaTab.Swap && <NovaSwap />}
+          {userAddress && tabState == NovaTab.Pool && <NovaPool />}
+          {userAddress && tabState == NovaTab.Send && <NovaSend />}
+          {userAddress && tabState == NovaTab.NFTs && <NovaNFTs />}
           {!userAddress && (
             <div className="flex flex-col border-4 bg-opacity-50 border-gray-600 bg-gray-400 border-opacity-50 rounded-md items-center justify-center h-[50vh] w-[50vw] p-4">
               <h2 className="text-2xl pb-3 font-bold">NOVA</h2>
