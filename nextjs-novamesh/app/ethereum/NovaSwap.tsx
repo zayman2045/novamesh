16db1af6f93cc4cd2af58f4c2a43b7c1ec9b6a02
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-23 19:20:34 -0400

    Add UI for checking the user's ETH balance and displaying transaction status

--- nextjs-novamesh/app/ethereum/NovaSwap.tsx
+++ nextjs-novamesh/app/ethereum/NovaSwap.tsx
@@ -1,4 +1,6 @@
-import { useAccount } from "wagmi";
+import { useAccount, useBalance } from "wagmi";
+import { useState } from "react";
+import { sepolia } from "wagmi/chains";
 import {
   useReadNovaTokenBalanceOf,
   useWriteNovaTokenMintTokens,
@@ -9,15 +11,27 @@ export default function NovaSwap() {
   // Get the user's address
   const { address: userAddress } = useAccount();
 
-  // Get the Nova balance of the user
-  const { data: userNovaBalance } = useReadNovaTokenBalanceOf({
+  // Get the ETH balance of the user;
+  const { data: userEthBalanceData } = useBalance({
+    address: userAddress,
+    chainId: sepolia.id,
+  });
+
+  // Check if userEthBalance is defined before converting to a number
+  const userEthBalance =
+    userEthBalanceData !== undefined
+      ? (Number(userEthBalanceData.value) / 10 ** 18).toFixed(4)
+      : 0;
+
+  // Get the NOVA balance of the user
+  const { data: userNovaBalanceData } = useReadNovaTokenBalanceOf({
     args: [userAddress!],
   });
 
   // Check if userNovaBalance is defined before converting to a number
-  const userNovaBalanceNumber =
-    userNovaBalance !== undefined
-      ? (Number(userNovaBalance) / 10 ** 18).toFixed(4)
+  const userNovaBalance =
+    userNovaBalanceData !== undefined
+      ? (Number(userNovaBalanceData) / 10 ** 18).toFixed(4)
       : 0;
 
   // Destructure the returned values from the useWriteNovaTokenMintTokens hook
@@ -27,36 +41,72 @@ export default function NovaSwap() {
     status, // Function to call the mintTokens method on the contract
   } = useWriteNovaTokenMintTokens();
 
-  // Mint Nova tokens to the user
+  // Define message state
+  const [message, setMessage] = useState("");
+
+  // Mint Nova tokens to the user when the form is submitted
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const novaTokenAmount = formData.get("novaTokenAmount") as string;
-    mintTokens({ value: parseEther(novaTokenAmount) });
+    if (Number(novaTokenAmount) > Number(userEthBalance)) {
+      setMessage("Insufficient Eth Balance");
+      return;
+    } else {
+      setMessage("");
+      mintTokens({ value: parseEther(novaTokenAmount) });
+    }
+  };
+
+  const getStatusMessage = (status: string) => {
+    switch (status) {
+      case "pending":
+        return "Processing transaction...";
+      case "error":
+        return "Error minting tokens";
+      case "success":
+        return "Tokens minted successfully";
+      case "idle":
+        return null;
+      default:
+        return null;
+    }
   };
 
   return (
-      <div className="flex flex-col border-4 bg-opacity-50 border-opacity-50 rounded-md items-center justify-center h-[50vh] w-[50vw] p-4  border-custom-blue bg-blue-400">
-          <div>
-            <h3 className="mb-3">
-              Nova Balance: {userNovaBalanceNumber.toString()}
-            </h3>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <input
-              name="novaTokenAmount"
-              type="text"
-              className="text-black mr-3 text-right"
-              placeholder="0"
-              required
-            ></input>
-            <button
-              type="submit"
-              className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105"
-            >
-              Mint
-            </button>
-          </form>
+    <div className="flex flex-col border-4 bg-opacity-50 border-opacity-50 rounded-md items-center justify-center h-[50vh] w-[50vw] p-4  border-custom-blue bg-blue-400">
+      <div>
+        <h3 className="mb-3">Nova Balance: {userNovaBalance.toString()}</h3>
+        <h3>Eth Balance: {userEthBalance.toString()}</h3>
       </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="novaTokenAmount"
+          type="text"
+          className="text-black mr-3 text-right"
+          placeholder="0"
+          required
+        ></input>
+        <button
+          type="submit"
+          className="bg-custom-blue p-1 border border-custom-blue rounded-lg w-20 font-bold hover:scale-105"
+        >
+          Mint
+        </button>
+      </form>
+      <div>{message}</div>
+      <div>
+        {hash && (
+          <a
+            href={`https://sepolia.etherscan.io/tx/${hash}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            View Transaction
+          </a>
+        )}
+      </div>
+      <div>{getStatusMessage(status)}</div>
+    </div>
   );
 }
