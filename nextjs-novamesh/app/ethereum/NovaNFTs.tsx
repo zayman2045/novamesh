ee67c2139e28c216e1e7dc9946c19107a6de4892
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-24 14:17:35 -0400

    Update the border of the NovaSend and NovaNFTs components

--- nextjs-novamesh/app/ethereum/NovaNFTs.tsx
+++ nextjs-novamesh/app/ethereum/NovaNFTs.tsx
@@ -1,5 +1,5 @@
 export default function NovaNFTs() {
   return (
-    <div className="flex flex-col border-4 bg-opacity-50 border-opacity-50 rounded-md items-center justify-center h-[50vh] w-[50vw] p-4  border-custom-blue bg-blue-400"></div>
+    <div className="flex flex-col border-4 bg-opacity-50 border-opacity-50 rounded-3xl items-center justify-center h-[50vh] w-[50vw] p-4  border-custom-blue bg-blue-400"></div>
   );
 }
