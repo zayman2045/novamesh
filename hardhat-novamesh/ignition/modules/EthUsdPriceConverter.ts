d2beb739fe92ff8bc37a49f1c8128e57834b6979
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-25 18:16:15 -0400

    Define a hardhat ignition deployment module for the EthUsdPriceConverter contract

--- hardhat-novamesh/ignition/modules/EthUsdPriceConverter.ts
+++ hardhat-novamesh/ignition/modules/EthUsdPriceConverter.ts
@@ -1,7 +1,12 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 // Build a hardhat ignition module to deploy to EthUsdPriceConverter contract
-export default buildModule("EthUsdPriceConverterModule", (module) => {
+const EthUsdPriceConverterModule = buildModule(
+  "EthUsdPriceConverterModule",
+  (module) => {
     const ethUsdPriceConverter = module.contract("EthUsdPriceConverter", []);
-    return { contract: ethUsdPriceConverter };
-});
+    return { ethUsdPriceConverter };
+  }
+);
+
+export default EthUsdPriceConverterModule;
