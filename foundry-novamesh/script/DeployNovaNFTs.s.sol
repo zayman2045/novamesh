e5e08812b98dc42b00e7f44634a74ce47862cf28
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-26 20:55:56 -0400

    Rename the NovaNFT deployment script file

--- foundry-novamesh/script/DeployNovaNFTs.s.sol
+++ foundry-novamesh/script/DeployNovaNFTs.s.sol
@@ -3,13 +3,13 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
-import {EthUsdPriceConverter} from "../src/EthUsdPriceConverter.sol";
+import {NovaNFTs} from "../src/NovaNFTs.sol";
 
-contract DeployEthUsdPriceConverter is Script {
+contract DeployNovaNFTs is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("SEPOLIA_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        new EthUsdPriceConverter();
+        new NovaNFTs();
         vm.stopBroadcast();
     }
 }
