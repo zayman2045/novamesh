48027b4ffb5eec03fd3b23abcc76cb4a0b1f3a76
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-26 20:58:42 -0400

    Update the EthUsdPriceConverter test file to use the deployment script and the ChainConfig script

--- foundry-novamesh/test/EthUsdPriceConverter.t.sol
+++ foundry-novamesh/test/EthUsdPriceConverter.t.sol
@@ -4,17 +4,20 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {EthUsdPriceConverter} from "../src/EthUsdPriceConverter.sol";
+import {ChainConfig} from "../script/ChainConfig.s.sol";
+import {DeployEthUsdPriceConverter} from "../script/DeployEthUsdPriceConverter.s.sol";
 
 contract EthUsdPriceConverterTest is Test {
-   EthUsdPriceConverter converter;
+    EthUsdPriceConverter priceConverter;
 
-   function setUp() public {
-    converter = new EthUsdPriceConverter();
-   }
+    function setUp() public {
+        DeployEthUsdPriceConverter deployEthUsdPriceConverter = new DeployEthUsdPriceConverter();
+        priceConverter = deployEthUsdPriceConverter.run();
+    }
 
-   function testGetDecimals() public view {
-    uint256 acutalDecimals = converter.getDecimals();
-    uint256 expectedDecimals = 8;
-    assertEq(acutalDecimals, expectedDecimals);
-   }
+    function testGetDecimals() public view {
+        uint256 acutalDecimals = priceConverter.getDecimals();
+        uint256 expectedDecimals = 8;
+        assertEq(acutalDecimals, expectedDecimals);
+    }
 }
