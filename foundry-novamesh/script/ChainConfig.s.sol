ea31c75a6db97f1512ca6c5a1cffee6c2fd01fde
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-26 21:54:38 -0400

    Update the ChainConfig for blockchain agnostic testing

--- foundry-novamesh/script/ChainConfig.s.sol
+++ foundry-novamesh/script/ChainConfig.s.sol
@@ -3,9 +3,13 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract ChainConfig is Script {
     NetworkConfig public activeNetworkConfig;
+
+    uint8 constant public DECIMALS = 8;
+    int256 constant public INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -15,7 +19,7 @@ contract ChainConfig is Script {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -24,16 +28,25 @@ contract ChainConfig is Script {
         pure
         returns (NetworkConfig memory)
     {
-        NetworkConfig memory sepoliaEthConfig = NetworkConfig({
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
-        return sepoliaEthConfig;
+        return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory anvilEthConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
         });
-        return anvilEthConfig;
+
+        return anvilConfig;
     }
 }
