1544a033fc0584e45823928d4606c56ec8355b58
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-28 23:07:43 -0400

    Make the NFT_PRICE a public variable, and define a getter function for the baseURI

--- hardhat-novamesh/contracts/NovaNFTs.sol
+++ hardhat-novamesh/contracts/NovaNFTs.sol
@@ -5,15 +5,11 @@ pragma solidity ^0.8.24;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NovaNFTs is ERC721 {
-     uint256 private s_tokenCounter;
-    uint256 private constant NFT_PRICE = 0.01 ether;
+    uint256 private s_tokenCounter;
+    uint256 public constant NFT_PRICE = 0.01 ether;
     string private s_baseURI = "https://ipfs.io/ipfs/";
 
     constructor() ERC721("Nova NFTs", "NNFT") {}
-
-    function _baseURI() internal view override returns (string memory) {
-        return s_baseURI;
-    }
 
     function mintNFT(address recipient) public payable returns (uint256) {
         require(msg.value == NFT_PRICE, "Incorrect Ether value sent");
@@ -21,5 +17,13 @@ contract NovaNFTs is ERC721 {
         _safeMint(recipient, newItemId);
         s_tokenCounter += 1;
         return newItemId;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return s_baseURI;
+    }
+
+    function getBaseURI() external view returns (string memory) {
+        return s_baseURI;
     }
 }
