035aec77ce604a25b1850d4469bd57d161d60a97
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-28 21:40:11 -0400

    Fix a typo in the comment above the module

--- hardhat-novamesh/ignition/modules/NovaToken.ts
+++ hardhat-novamesh/ignition/modules/NovaToken.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-// Build a hardhat ignition module to deploy to NovaToken contract
+// Build a hardhat ignition module to deploy the NovaToken contract
 const NovaTokenModule = buildModule("NovaTokenModule", (m) => {
   const novaToken = m.contract("NovaToken");
   return { novaToken };
