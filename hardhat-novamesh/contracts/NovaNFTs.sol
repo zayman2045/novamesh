7086de68f84c5f79264aa74a4a2b6bb72cde3f4c
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-28 20:25:31 -0400

    Define a NovaNFT contract within the hardhat project directory

---
+++ hardhat-novamesh/contracts/NovaNFTs.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NovaNFTs is ERC721 {
+     uint256 private s_tokenCounter;
+    uint256 private constant NFT_PRICE = 0.01 ether;
+    string private s_baseURI = "https://ipfs.io/ipfs/";
+
+    constructor() ERC721("Nova NFTs", "NNFT") {}
+
+    function _baseURI() internal view override returns (string memory) {
+        return s_baseURI;
+    }
+
+    function mintNFT(address recipient) public payable returns (uint256) {
+        require(msg.value == NFT_PRICE, "Incorrect Ether value sent");
+        uint256 newItemId = s_tokenCounter;
+        _safeMint(recipient, newItemId);
+        s_tokenCounter += 1;
+        return newItemId;
+    }
+}
