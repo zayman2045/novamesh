eb398cb799857ddbea1e3ce8877581374e20b6c6
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-28 23:07:24 -0400

    Update the NovaNFTs contract with up to date name and symbol values

--- foundry-novamesh/test/NovaNFTs.t.sol
+++ foundry-novamesh/test/NovaNFTs.t.sol
@@ -6,7 +6,6 @@ import {Test} from "forge-std/Test.sol";
 import {NovaNFTs} from "../src/NovaNFTs.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
 
-
 contract NovaNFTsTest is Test {
     NovaNFTs nft;
 
@@ -26,31 +25,33 @@ contract NovaNFTsTest is Test {
     }
 
     function test_BaseURI() public view {
-        string memory uri = nft.baseURI();
+        string memory uri = nft.getBaseURI();
         assertEq(uri, "https://ipfs.io/ipfs/");
     }
 
     function test_TokenPrice() public view {
-        uint256 price = nft.tokenPrice();
+        uint256 price = nft.NFT_PRICE();
         assertEq(price, 0.01 ether);
     }
 
     function test_Name() public view {
         string memory name = nft.name();
-        assertEq(name, "NovaNFTs");
+        assertEq(name, "Nova NFTs");
     }
 
     function test_Symbol() public view {
         string memory symbol = nft.symbol();
-        assertEq(symbol, "NOVA");
+        assertEq(symbol, "NNFT");
     }
 
     function test_TokenURI() public {
         address recipient = address(0x123);
         uint256 tokenId = nft.mintNFT{value: 0.01 ether}(recipient);
-        string memory baseURI = nft.baseURI();
+        string memory baseURI = nft.getBaseURI();
         string memory tokenURI = nft.tokenURI(tokenId);
-        string memory expectedTokenURI = string(abi.encodePacked(baseURI, Strings.toString(tokenId)));
+        string memory expectedTokenURI = string(
+            abi.encodePacked(baseURI, Strings.toString(tokenId))
+        );
         assertEq(tokenURI, expectedTokenURI);
     }
 }
