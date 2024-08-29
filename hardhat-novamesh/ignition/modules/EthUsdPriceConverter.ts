2b00aab2cb732c92b73a1631821392f96d5cc603
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-28 21:40:03 -0400

    Fix a typo in the comment above the module

--- hardhat-novamesh/ignition/modules/EthUsdPriceConverter.ts
+++ hardhat-novamesh/ignition/modules/EthUsdPriceConverter.ts
@@ -1,10 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-// Build a hardhat ignition module to deploy to EthUsdPriceConverter contract
+// Build a hardhat ignition module to deploy the EthUsdPriceConverter contract
 const EthUsdPriceConverterModule = buildModule(
   "EthUsdPriceConverterModule",
   (module) => {
-    const ethUsdPriceConverter = module.contract("EthUsdPriceConverter", []);
+    const ethUsdPriceConverter = module.contract("EthUsdPriceConverter");
     return { ethUsdPriceConverter };
   }
 );
