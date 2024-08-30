484d6f926e999ec7b418a06f66df58832e02a020
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-29 22:56:55 -0400

    Remove unused Hardhat files and directories

--- hardhat-novamesh/ignition/modules/NovaToken.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-// Build a hardhat ignition module to deploy the NovaToken contract
-const NovaTokenModule = buildModule("NovaTokenModule", (m) => {
-  const novaToken = m.contract("NovaToken");
-  return { novaToken };
-});
-
-export default NovaTokenModule;
