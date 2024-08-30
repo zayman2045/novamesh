fb46031266e7e47d3582c86a655a7058ad170753
zayman2045 <xavierorlandogriffith@gmail.com>
2024-08-29 21:28:37 -0400

    Import hardhat tests

---
+++ foundry-novamesh/test/hardhat-tests/NovaToken.ts
@@ -0,0 +1,33 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { ignition, ethers } from "hardhat";
+import NovaTokenModule from "../../ignition/modules/NovaToken";
+import { expect } from "chai";
+
+describe("nova token", () => {
+  const setup = async () => {
+    // Deploy the NovaToken contract
+    const deployment = await ignition.deploy(NovaTokenModule);
+
+    // Get the NovaToken contract from ethers
+    const novaToken = await ethers.getContractAt(
+      "NovaToken",
+      deployment.novaToken
+    );
+
+    // Get the signer
+    const [signer] = await ethers.getSigners()
+    return { novaToken, signer };
+  };
+
+  it("should mint Nova tokens in exchange for ETH", async () => {
+    // Get the NovaToken contract and the signer
+    const { novaToken, signer } = await loadFixture(setup);
+
+    //Check initial balance
+    expect(await novaToken.balanceOf(signer)).to.equal(0);
+
+    //Mint tokens and check balance after purchase
+    await novaToken.mintTokens({value: 1});
+    expect(await novaToken.balanceOf(signer)).to.equal(1);
+  });
+});
