45a981c2cee75feb0b29a0406ad9cf2a48990063
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-03 19:08:32 -0400

    Removed the unused the import

--- ethereum-contracts/test/EthUsdPriceConverter.t.sol
+++ ethereum-contracts/test/EthUsdPriceConverter.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {EthUsdPriceConverter} from "../src/EthUsdPriceConverter.sol";
-import {ChainConfig} from "../script/ChainConfig.s.sol";
 import {DeployEthUsdPriceConverter} from "../script/DeployEthUsdPriceConverter.s.sol";
 
 contract EthUsdPriceConverterTest is Test {
