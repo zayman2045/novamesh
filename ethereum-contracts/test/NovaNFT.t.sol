247a6371dd560f8671a710885f604d55cb799b1d
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-03 19:07:51 -0400

    Rename the test suite file for the NovaNFTs smart contract

--- ethereum-contracts/test/NovaNFT.t.sol
+++ ethereum-contracts/test/NovaNFT.t.sol
@@ -3,52 +3,55 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
-import {NovaNFTs} from "../src/NovaNFTs.sol";
+import {NovaNFT} from "../src/NovaNFT.sol";
+import {DeployNovaNFT} from "../script/DeployNovaNFT.s.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract NovaNFTsTest is Test {
-    NovaNFTs nft;
+    NovaNFT novaNFT;
+
 
     function setUp() public {
-        nft = new NovaNFTs();
+        DeployNovaNFT deployNovaNFT = new DeployNovaNFT();
+        novaNFT = deployNovaNFT.run();
     }
 
     function test_MintNFT() public {
         address recipient = address(0x123);
-        uint256 tokenId = nft.mintNFT{value: 0.01 ether}(recipient);
+        uint256 tokenId = novaNFT.mintNFT{value: 0.01 ether}(recipient);
         assertEq(tokenId, 0);
     }
 
     function testFail_MintNFTWithIncorrectValue() public {
         address recipient = address(0x123);
-        nft.mintNFT{value: 0.001 ether}(recipient);
+        novaNFT.mintNFT{value: 0.001 ether}(recipient);
     }
 
     function test_BaseURI() public view {
-        string memory uri = nft.getBaseURI();
+        string memory uri = novaNFT.getBaseURI();
         assertEq(uri, "https://ipfs.io/ipfs/");
     }
 
     function test_TokenPrice() public view {
-        uint256 price = nft.NFT_PRICE();
+        uint256 price = novaNFT.TOKEN_PRICE();
         assertEq(price, 0.01 ether);
     }
 
     function test_Name() public view {
-        string memory name = nft.name();
-        assertEq(name, "Nova NFTs");
+        string memory name = novaNFT.name();
+        assertEq(name, "Nova NFT");
     }
 
     function test_Symbol() public view {
-        string memory symbol = nft.symbol();
+        string memory symbol = novaNFT.symbol();
         assertEq(symbol, "NNFT");
     }
 
     function test_TokenURI() public {
         address recipient = address(0x123);
-        uint256 tokenId = nft.mintNFT{value: 0.01 ether}(recipient);
-        string memory baseURI = nft.getBaseURI();
-        string memory tokenURI = nft.tokenURI(tokenId);
+        uint256 tokenId = novaNFT.mintNFT{value: 0.01 ether}(recipient);
+        string memory baseURI = novaNFT.getBaseURI();
+        string memory tokenURI = novaNFT.tokenURI(tokenId);
         string memory expectedTokenURI = string(
             abi.encodePacked(baseURI, Strings.toString(tokenId))
         );
