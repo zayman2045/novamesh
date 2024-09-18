bcc255967ca8f28b39a4ff6a2de3df35ddbdb59c
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-17 22:09:32 -0400

    Rename the NovaNFT ignition module and pass in the subscriptionId as a BigInt

--- ethereum-contracts/ignition/modules/NovaNFTs.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-// Build a hardhat ignition module to deploy the NovaNFTs contract
-const NovaNftsModule = buildModule("NovaNftsModule", (m) => {
-  const novaNfts = m.contract("NovaNFTs");
-  return { novaNfts };
-});
-
-export default NovaNftsModule;
