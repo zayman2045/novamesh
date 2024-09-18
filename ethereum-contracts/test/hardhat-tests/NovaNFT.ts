58fcf0cffc562f9270c83e2af5f36f8a0eb03182
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-17 22:10:03 -0400

    Rename the NovaNFT contract in the hardhat test file

--- ethereum-contracts/test/hardhat-tests/NovaNFT.ts
+++ ethereum-contracts/test/hardhat-tests/NovaNFT.ts
@@ -1,20 +1,20 @@
 import {ignition, ethers} from 'hardhat';
-import NovaNftsModule from '../../ignition/modules/NovaNFTs';
+import NovaNftsModule from '../../ignition/modules/NovaNFT';
 import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from 'chai';
 
-describe("nova nfts", () => {
+describe("nova nft", () => {
     async function setup() {
         const deployment = await ignition.deploy(NovaNftsModule);
-        const novaNFTs = await ethers.getContractAt("NovaNFTs", deployment.novaNfts);
+        const novaNFT = await ethers.getContractAt("NovaNFT", deployment.novaNft);
         const [signer] = await ethers.getSigners();
-        return { novaNFTs, signer };
+        return { novaNFT, signer };
     }
     
         it("should mint a new NFT to the recipient address", async () => {
-        const { novaNFTs, signer } = await loadFixture(setup);
-        const tx = await novaNFTs.mintNFT(signer.address, { value: ethers.parseEther("0.01") });
+        const { novaNFT, signer } = await loadFixture(setup);
+        const tx = await novaNFT.mintNFT(signer.address, { value: ethers.parseEther("0.01") });
         await tx.wait(); // Wait for the transaction to be mined
-        expect(await novaNFTs.ownerOf(0)).to.equal(signer.address);
+        expect(await novaNFT.ownerOf(0)).to.equal(signer.address);
     });
 })
