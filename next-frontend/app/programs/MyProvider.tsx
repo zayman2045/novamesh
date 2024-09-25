ef5768fd82470aaf21c33adc068839cc61eb7598
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-25 02:06:20 -0400

    Remove unused components

--- next-frontend/app/programs/MyProvider.tsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, ReactNode } from "react";
-
-const MyContext = createContext("hello");
-
-const MyProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <MyContext.Provider value={"goodbye"}>{children}</MyContext.Provider>;
-};
-
-export { MyContext, MyProvider };
