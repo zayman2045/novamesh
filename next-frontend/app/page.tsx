6dcec782677948cd0c91155f4f94b0e54aac5f8b
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-25 02:06:58 -0400

    Define CSS classes for hiding and revealing HTML elements

--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,38 +1,51 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import ethLogo from "@/public/ethereum-eth-logo.svg";
 import solLogo from "@/public/solana-sol-logo.png";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  const [isScrolled, setIsScrolled] = useState(false);
-
   useEffect(() => {
-    const handleScroll = () => {
-      const position = window.scrollY;
-      setIsScrolled(position > 275);
-    };
+    const hiddenElements = document.querySelectorAll(".hide");
 
-    window.addEventListener("scroll", handleScroll);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+          } else {
+            entry.target.classList.remove("show");
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
 
+    hiddenElements.forEach((element) => {
+      observer.observe(element);
+    });
+
+    // Cleanup observer on component unmount
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      hiddenElements.forEach((element) => {
+        observer.unobserve(element);
+      });
     };
   }, []);
 
   return (
     <div className="flex flex-col m-10">
-      <div
-        className={`flex flex-col justify-center items-center text-center h-screen transition-all duration-500 ${isScrolled ? "blur-sm" : ""}`}
-      >
+      <div className="flex flex-col justify-center items-center text-center h-[calc(100vh-4rem)] hide">
         <h1 className="text-3xl font-bold">Welcome to NovaMesh</h1>
         <p className="m-6">
           Connect your Ethereum and Solana Wallet to mint Tokens and NFTs
         </p>
       </div>
-      <div className="flex justify-evenly items-center my-6 min-h-[80vh]">
+      <div className="flex justify-evenly items-center my-6 h-[calc(100vh-4rem)] hide">
         <div className="w-1/6 p-2 hover:scale-110 transition-transform duration-300">
           <Link href="/ethereum">
             <Image src={ethLogo} alt="ETH Logo" className="w-full" />
