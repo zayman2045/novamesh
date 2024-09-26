1ae062a40db5142874b580a55c37f2adb7be519e
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-25 23:47:24 -0400

    Add the MintToken component to the Solana homepage

--- next-frontend/app/solana/page.tsx
+++ next-frontend/app/solana/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useWallet } from "@solana/wallet-adapter-react";
+import MintToken from "./MintToken";
 
 export default function SolanaHome() {
   const { connected, publicKey } = useWallet();
@@ -12,13 +13,13 @@ export default function SolanaHome() {
           className={`flex flex-col items-center justify-center border-4 ${connected ? "border-custom-purple" : "border-gray-600"} rounded-md m-4 p-4 h-1/2 w-1/2 ${connected ? "bg-custom-purple" : "bg-gray-400"} bg-opacity-50 border-opacity-50`}
         >
           {connected ? (
-            <button className="bg-custom-purple w-20 font-bold border border-custom-purple rounded-lg p-1 hover:scale-105">
-              Swap
-            </button>
+            <>
+              <MintToken />
+            </>
           ) : (
             <>
-            <h2 className="text-2xl font-bold pb-3">Mesh</h2>
-            <p>Connect Wallet to Access</p>
+              <h2 className="text-2xl font-bold pb-3">Mesh</h2>
+              <p>Connect Wallet to Access</p>
             </>
           )}
         </div>
