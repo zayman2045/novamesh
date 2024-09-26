b695b3cf4919ac46d3c3503fab5cc04cb8e7f565
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-25 23:58:00 -0400

    Import types from @solana packages

--- next-frontend/app/solana/MeshTokens.tsx
+++ next-frontend/app/solana/MeshTokens.tsx
@@ -1,21 +1,39 @@
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  transfer,
+  Account,
+  getMint,
+  getAccount,
+} from "@solana/spl-token";
+
 export default function MeshTokens() {
   return (
     <>
-    <h2 className="text-2xl font-bold pb-3">Mesh Tokens</h2>
-    <div className="flex justify-between w-full py-2">
-      <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
-        Create Token
-      </button>
-      <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
-        Mint Token
-      </button>
-      <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
-        Check Balance
-      </button>
-      <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
-        Send Token
-      </button>
-    </div>
+      <h2 className="text-2xl font-bold pb-3">Mesh Tokens</h2>
+      <div className="flex justify-between w-full py-2">
+        <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
+          Create Token
+        </button>
+        <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
+          Mint Token
+        </button>
+        <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
+          Check Balance
+        </button>
+        <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
+          Send Token
+        </button>
+      </div>
     </>
   );
 }
