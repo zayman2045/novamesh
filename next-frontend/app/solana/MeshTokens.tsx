8a9a657fee13f4837d41d62b306964df145b168f
zayman2045 <xavierorlandogriffith@gmail.com>
2024-09-25 23:50:49 -0400

    Rename MintToken to MeshTokens

--- next-frontend/app/solana/MeshTokens.tsx
+++ next-frontend/app/solana/MeshTokens.tsx
@@ -1,5 +1,7 @@
-export default function MintToken() {
+export default function MeshTokens() {
   return (
+    <>
+    <h2 className="text-2xl font-bold pb-3">Mesh Tokens</h2>
     <div className="flex justify-between w-full py-2">
       <button className="border border-custom-purple rounded-md hover:scale-110 transition-transform p-1 font-bold bg-custom-purple mx-1 w-1/5">
         Create Token
@@ -14,5 +16,6 @@ export default function MintToken() {
         Send Token
       </button>
     </div>
+    </>
   );
 }
