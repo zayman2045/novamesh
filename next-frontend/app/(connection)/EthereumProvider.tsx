e6472ae79a1dd4530f84c638f25c63e603e4181c
zayman2045 <xavierorlandogriffith@gmail.com>
2024-10-09 17:19:05 -0400

    Add comments to the functional component

--- next-frontend/app/(connection)/EthereumProvider.tsx
+++ next-frontend/app/(connection)/EthereumProvider.tsx
@@ -7,19 +7,20 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
 import { mainnet, sepolia } from "wagmi/chains";
 
+// Create a new QueryClient instance
 const queryClient = new QueryClient();
 
 interface EthereumProviderProps {
-  children: ReactNode;
-  projectId: string;
+  children: ReactNode; // The children of the component
+  projectId: string; // The Wallet Connect project ID
 }
 
 const EthereumProvider: FC<EthereumProviderProps> = ({ children, projectId }) => {
   const config = getDefaultConfig({
-    appName: "NovaMesh",
-    projectId,
-    chains: [mainnet, sepolia],
-    ssr: true,
+    appName: "NovaMesh", // Set the app name
+    projectId, // Set the Wallet Connect project ID
+    chains: [mainnet, sepolia], // Add the mainnet and sepolia chains
+    ssr: true, // Enable server-side rendering
   });
 
   return (
