a28e6b55968fe1c9f7374ce761a7c33c3d61932f
zayman2045 <xavierorlandogriffith@gmail.com>
2024-10-09 20:53:00 -0400

    Update the Solana homepage with conditional rendering of the application UI

--- next-frontend/app/solana/page.tsx
+++ next-frontend/app/solana/page.tsx
@@ -2,25 +2,43 @@
 
 import { useWallet } from "@solana/wallet-adapter-react";
 import MeshSwap from "./MeshSwap";
+import MeshTabButton from "./MeshTabButton";
+import { MeshTab, useMeshTab } from "./MeshTabProvider";
+import { useEffect } from "react";
 
 export default function SolanaHome() {
+  // Get the browser wallet
   const { connected, publicKey } = useWallet();
+
+  // Get the current tab and the function to set the tab
+  const { tab: tabState, setTab } = useMeshTab();
+
+  useEffect(() => {
+    // Set the tab to Swap if the user is connected
+    if (connected) {
+      setTab(MeshTab.Swap);
+    } else {
+      setTab(MeshTab.None);
+    }
+  }, [connected]);
 
   return (
     <>
       <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)] m-4 text-white">
-        <div
-          className={`flex flex-col items-center justify-center border-4 ${connected ? "border-custom-purple" : "border-gray-600"} rounded-md m-4 p-4 h-1/2 w-1/2 ${connected ? "bg-custom-purple" : "bg-gray-400"} bg-opacity-50 border-opacity-50`}
-        >
-          {connected ? (
-            <>
-              <MeshSwap />
-            </>
-          ) : (
-            <>
-              <h2 className="text-2xl font-bold pb-3">Mesh</h2>
+        <div className="h-[55vh]">
+          <div>
+            <MeshTabButton tabValue={MeshTab.Swap} />
+            <MeshTabButton tabValue={MeshTab.NFTs} />
+            <MeshTabButton tabValue={MeshTab.Send} />
+          </div>
+          {connected && tabState == MeshTab.Swap && <MeshSwap />}
+          {connected && tabState == MeshTab.Send && <MeshSwap />}
+          {connected && tabState == MeshTab.NFTs && <MeshSwap />}
+          {!connected && (
+            <div className="flex flex-col border-4 bg-opacity-50 border-gray-600 bg-gray-400 border-opacity-50 rounded-3xl items-center justify-center h-[50vh] w-[50vw] p-4">
+              <h2 className="text-2xl pb-3 font-bold">NOVA</h2>
               <p>Connect Wallet to Access</p>
-            </>
+            </div>
           )}
         </div>
       </div>
