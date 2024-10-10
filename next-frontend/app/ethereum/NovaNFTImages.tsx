77f76622564d25ac6853d18f7a08aeb28df88900
zayman2045 <xavierorlandogriffith@gmail.com>
2024-10-09 21:50:32 -0400

    Add an image to the NovaNFTImages component

--- next-frontend/app/ethereum/NovaNFTImages.tsx
+++ next-frontend/app/ethereum/NovaNFTImages.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import NovaNft1 from "@/public/nova-nft-1.jpeg";
 import NovaNft2 from "@/public/nova-nft-2.jpeg";
 import NovaNft3 from "@/public/nova-nft-3.jpeg";
+import NovaNft4 from "@/public/nova-nft-4.jpeg";
 
 export default function NovaNFTImages() {
   return (
@@ -14,6 +15,9 @@ export default function NovaNFTImages() {
       </div>
       <div className="flex flex-col items-center justify-center min-w-[33%]">
         <Image src={NovaNft3} alt="Nova NFT 3" className="rounded-lg" />
+      </div>
+      <div className="flex flex-col items-center justify-center min-w-[33%]">
+        <Image src={NovaNft4} alt="Nova NFT 4" className="rounded-lg" />
       </div>
     </>
   );
