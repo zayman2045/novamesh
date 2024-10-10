0c84fbbe40197f7c6b9d65b8b431770f9daa6875
zayman2045 <xavierorlandogriffith@gmail.com>
2024-10-09 21:49:47 -0400

    Remove the placeholder text in the HomeContent component

--- next-frontend/app/HomeContent.tsx
+++ next-frontend/app/HomeContent.tsx
@@ -17,11 +17,11 @@ export default function HomeContent({
             <h1 className="text-2xl font-bold">{title}</h1>
             <p>{description}</p>
           </div>
-          <div className="h-52 w-52 border border-blue-400">Image</div>
+          <div className="h-52 w-52 border border-blue-400"></div>
         </>
       ) : (
         <>
-          <div className="h-52 w-52 border border-blue-400">Image</div>
+          <div className="h-52 w-52 border border-blue-400"></div>
           <div className="w-1/2 flex flex-col justify-around h-1/2">
             <h1 className="text-2xl font-bold">{title}</h1>
             <p>{description}</p>
