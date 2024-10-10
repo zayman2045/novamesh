f469a7e16394ac83644f5655edaf2fe1d4c3844d
zayman2045 <xavierorlandogriffith@gmail.com>
2024-10-10 14:32:36 -0400

    Update the HomeContent functional component with the homepage image

--- next-frontend/app/HomeContent.tsx
+++ next-frontend/app/HomeContent.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import HomeArt1 from "@/public/home-art-1.png";
+
 interface HomeContentProps {
   title: string; // The title to be displayed in the component
   description: string; // The description to be displayed in the component
@@ -10,18 +13,26 @@ export default function HomeContent({
   orientation,
 }: HomeContentProps) {
   return (
-    <div className="flex justify-around items-center hide h-52">
+    <div className="flex justify-around items-center hide h-64">
       {orientation === "left" ? (
         <>
           <div className="w-1/2 flex flex-col justify-around h-1/2">
             <h1 className="text-2xl font-bold">{title}</h1>
             <p>{description}</p>
           </div>
-          <div className="h-52 w-52 border border-blue-400"></div>
+          <Image
+            src={HomeArt1}
+            alt="Home Art 1"
+            className="rounded-lg w-1/3 h-auto"
+          />
         </>
       ) : (
         <>
-          <div className="h-52 w-52 border border-blue-400"></div>
+          <Image
+            src={HomeArt1}
+            alt="Home Art 1"
+            className="rounded-lg w-1/3 h-auto"
+          />
           <div className="w-1/2 flex flex-col justify-around h-1/2">
             <h1 className="text-2xl font-bold">{title}</h1>
             <p>{description}</p>
