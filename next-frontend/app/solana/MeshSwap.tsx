d8bfe705bfa62a7b93612874672150815b743cc2
zayman2045 <xavierorlandogriffith@gmail.com>
2024-10-09 20:51:19 -0400

    Update the MeshSwap component UI

--- next-frontend/app/solana/MeshSwap.tsx
+++ next-frontend/app/solana/MeshSwap.tsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+
 export default function MeshSwap() {
-  return <>MeshSwap</>;
+  const [message, setMessage] = useState("Swap");
+
+  return (
+    <>
+      <form
+        onSubmit={() => {}}
+        className="flex flex-col gap-2 items-center justify-center h-[50vh] w-[50vw]"
+      >
+        <div className="border-4 bg-opacity-50 border-opacity-50 border-custom-purple bg-custom-purple rounded-3xl h-2/5 w-full p-3">
+          <h3>Sell</h3>
+          <input
+            name="solAmount"
+            type="text"
+            className={`text-white mr-3 bg-transparent rounded-3xl w-3/4 h-1/2 p-1 text-left text-3xl outline-none shadow-none placeholder:text-white`}
+            placeholder="0"
+            required
+            onChange={() => {}}
+            value={""}
+          ></input>
+          <div className="flex justify-between">
+            <p>{}</p>
+            <p>SOL Balance: {}</p>
+          </div>
+        </div>
+        <div className="border-4 bg-opacity-50 border-opacity-50 border-custom-purple bg-custom-purple rounded-3xl h-2/5 w-full p-3">
+          <h3>Buy</h3>
+          <input
+            name="meshAmount"
+            type="text"
+            className={`text-white mr-3 bg-transparent rounded-3xl w-3/4 h-1/2 p-1 text-left text-3xl outline-none shadow-none placeholder:text-white`}
+            placeholder="0"
+            required
+            onChange={() => {}}
+            value={""}
+          ></input>
+          <div className="flex justify-between">
+            <p>{}</p>
+            <p>MESH Balance: {}</p>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className={`bg-custom-purple bg-opacity-75 p-1 border-2 border-custom-purple rounded-3xl w-full h-1/5 font-bold hover:scale-105 duration-300 ${message == "Insufficient SOL Balance" && "bg-red-500 border-red-500"} ${message == "Enter an amount" && "bg-opacity-50 border-gray-600 bg-gray-400 border-opacity-50"}`}
+          disabled={message != "Swap"}
+        >
+          {message}
+        </button>
+      </form>
+    </>
+  );
 }
