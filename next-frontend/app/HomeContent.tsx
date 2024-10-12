f0413d09a2fefc61637daf2c5e966ee3bacee6a9
zayman2045 <xavierorlandogriffith@gmail.com>
2024-10-12 10:36:26 -0400

    Refactor HomeContent component to dynamically display images based on title

--- next-frontend/app/HomeContent.tsx
+++ next-frontend/app/HomeContent.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import HomeArt1 from "@/public/home-art-1.png";
+import HomeArt2 from "@/public/home-art-2.png";
+import HomeArt3 from "@/public/home-art-3.png";
 
 interface HomeContentProps {
   title: string; // The title to be displayed in the component
@@ -7,11 +9,21 @@ interface HomeContentProps {
   orientation: "left" | "right"; // The orientation of the content
 }
 
+// Mapping titles to their corresponding images
+const titleToImageMap: { [key: string]: StaticImageData } = {
+  "Tokens": HomeArt1,
+  "NFTs": HomeArt2,
+  "Transfers": HomeArt3,
+};
+
 export default function HomeContent({
   title,
   description,
   orientation,
 }: HomeContentProps) {
+  // Get the image based on the title
+  const art = titleToImageMap[title];
+
   return (
     <div className="flex justify-around items-center hide h-64">
       {orientation === "left" ? (
@@ -21,16 +33,16 @@ export default function HomeContent({
             <p>{description}</p>
           </div>
           <Image
-            src={HomeArt1}
-            alt="Home Art 1"
+            src={art}
+            alt={title}
             className="rounded-lg w-1/3 h-auto"
           />
         </>
       ) : (
         <>
           <Image
-            src={HomeArt1}
-            alt="Home Art 1"
+            src={art}
+            alt={title}
             className="rounded-lg w-1/3 h-auto"
           />
           <div className="w-1/2 flex flex-col justify-around h-1/2">
